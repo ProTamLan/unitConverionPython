d62804723c0f21f2b78880936bd7738851a2b650
Claro <75819639+ProTamLan@users.noreply.github.com>
2022-08-05 16:44:35 -0700

    Add files via upload

---
+++ UnitConversion.py
@@ -0,0 +1,11 @@
+#Conversion Program
+def Menu():
+    print('1. Miles to Kilometers')
+    print('2. Pounds to tons')
+    print('3. Lemon to Lime')
+Menu()
+
+def miles_km():
+    km = float(input('Enter Kilometers:'))
+    miles=
+miles_km()
